b6cade9c4a630b7cc4c68eaae2912022401cc9b3
jonayuji <futagawa0305@gmail.com>
2025-01-25 15:44:40 +0900

    [Add}calculator初期実装完了

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,33 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+  //プロパティ
   title = 'calculator';
+  display = '';
+
+  //ディスプレイへの追加処理
+  appendInput = (value: string): void => {
+    this.display += value;
+  }
+
+  //ディスプレイの表示内容クリア処理
+  clear = (): void => {
+    this.display = '';
+  }
+
+  //計算処理
+  calculate = (): void => {
+    try {
+      this.display = eval(this.display);
+    } catch(e) {
+      this.display = 'ERROR';
+    }
+  }
 }
