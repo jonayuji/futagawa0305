b3a0efb78e37fafa74a0b4194939e5f6b7946a42
jonayuji <futagawa0305@gmail.com>
2025-02-02 17:55:49 +0900

    [ADD]history-listコンポーネントの作成

---
+++ src/app/history-list/history-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-list',
+  imports: [],
+  templateUrl: './history-list.component.html',
+  styleUrl: './history-list.component.css'
+})
+export class HistoryListComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AngularFireModule } from "@angular/fire/compat"; //[Wip]angular/fireのインポート
+import { AngularFireAuthModule } from "@angular/fire/compat/auth"; //[Wip]fire/authentication認証のインポート
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
